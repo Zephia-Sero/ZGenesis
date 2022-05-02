20a2956fd2c40d422bfededd90f2e5d2640098fa
Zephyr Serotonin <29110377+Zephi-Sero@users.noreply.github.com>
2022-05-02 06:28:34 -0500

    Made event queue itself.

--- ZGenesis/Events/Event.cs
+++ ZGenesis/Events/Event.cs
@@ -1,6 +1,8 @@
 ﻿namespace ZGenesis.Events {
     public class Event {
-        public Event() { }
+        public Event() {
+            Patcher.EnqueueEvent(this);
+        }
         public override string ToString() => $"{GetType().Name}";
     }
 }
