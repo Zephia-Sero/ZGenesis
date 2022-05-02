eab6576c592b5a661adc91e0b1046ea71cd4341d
Zephyr Serotonin <29110377+Zephi-Sero@users.noreply.github.com>
2022-05-02 06:37:50 -0500

    Added CameraRotateEvent

---
+++ ZGenesis/BaseMod/MiscPatches.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ZGenesis.Attributes;
+using ZGenesis.Events;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ZGenesis.BaseMod {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Reflection will call these when patched.")]
+    internal static class MiscPatches {
+        // Miscellaneous patches
+        private readonly static ConstructorInfo c_CaneraRotateEvent = typeof(CameraRotateEvent).GetConstructor(new Type[] { });
+        [ModPatch("transpiler", "Assembly-CSharp", "CameraRotationControls.Rotate")]
+        private static IEnumerable<CodeInstruction> OnCameraRotate(IEnumerable<CodeInstruction> instructions) {
+            foreach(CodeInstruction instruction in instructions) {
+                if(instruction.opcode == OpCodes.Ret) {
+                    yield return new CodeInstruction(OpCodes.Newobj, c_CaneraRotateEvent);
+                    yield return new CodeInstruction(OpCodes.Pop);
+                }
+                yield return instruction;
+            }
+        }
+    }
+}
