99eca5578b688e5c7079fa1c14554c6e946585f8
Zephyr Serotonin <29110377+Zephi-Sero@users.noreply.github.com>
2022-05-04 16:58:48 -0500

    Added simple command registration.

---
+++ ZGenesis/Objects/ModdedCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZGenesis.Objects {
+    public abstract class ModdedCommand {
+        public bool safe = false;
+        public string help = "Someone did a dum-dum and didn't add a command help text";
+        public abstract void Execute();
+        protected void Message(string message) {
+            GlobalChat.WriteCommandMessage(message);
+        }
+    }
+}
---
+++ ZGenesis/Registry/CommandRegistry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZGenesis.Objects;
+
+namespace ZGenesis.Registry {
+    public static class CommandRegistry {
+        private static readonly Dictionary<string, ModdedCommand> commands = new Dictionary<string, ModdedCommand>();
+        public static void Register(string modname, string alias, ModdedCommand command) {
+            if(!commands.ContainsKey(alias)) {
+                commands.Add(alias, command);
+            } else {
+                Logger.Log(Logger.LogLevel.WARNING, modname, "Command with alias '{0}' already added!", alias);
+            }
+        }
+    }
+}
