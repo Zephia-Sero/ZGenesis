3c4240449ab8275b34cbbcb2f40b89c6e03c9560
Zephyr Serotonin <29110377+Zephi-Sero@users.noreply.github.com>
2022-05-04 20:51:45 -0500

    Implemented TryRun() + added arguments to command.execute()

--- ZGenesis/Objects/ModdedCommand.cs
+++ ZGenesis/Objects/ModdedCommand.cs
@@ -8,7 +8,7 @@ namespace ZGenesis.Objects {
     public abstract class ModdedCommand {
         public bool safe = false;
         public string help = "Someone did a dum-dum and didn't add a command help text";
-        public abstract void Execute();
+        public abstract void Execute(string[] args);
         protected void Message(string message) {
             GlobalChat.WriteCommandMessage(message);
         }
--- ZGenesis/Registry/CommandRegistry.cs
+++ ZGenesis/Registry/CommandRegistry.cs
@@ -8,6 +8,9 @@ using ZGenesis.Objects;
 namespace ZGenesis.Registry {
     public static class CommandRegistry {
         private static readonly Dictionary<string, ModdedCommand> commands = new Dictionary<string, ModdedCommand>();
+        private static ModdedCommand commandToConfirm = null;
+        private static string[] argsToConfirm = null;
+        private static string cmdNameToConfirm = "";
         public static void Register(string modname, string alias, ModdedCommand command) {
             if(!commands.ContainsKey(alias)) {
                 commands.Add(alias, command);
@@ -16,7 +19,48 @@ namespace ZGenesis.Registry {
             }
         }
         public static bool TryRun(string command) {
-            Logger.Log("Command attempt", command);
+            if(command[0] == '/') {
+                string[] args = command.Substring(1).Split(' ');
+                if(args[0] == "help") {
+                    if(args.Length > 1) {
+                        if(commands.TryGetValue(args[1], out ModdedCommand other)) {
+                            if(!other.safe) {
+                                GlobalChat.WriteCommandMessage("<color=red>This is an extremely dangerous command. Use with caution, only when testing.</color>");
+                            }
+                            GlobalChat.WriteCommandMessage(other.help);
+                        } else return false;
+                    } else {
+                        GlobalChat.WriteCommandMessage("This is a list of all MODDED commands. Those marked in red are dangerous, and can permanently make your session unplayable. Run /help commandname to get more information about what a command does.");
+                        foreach(KeyValuePair<string, ModdedCommand> keyValuePair in commands) {
+                            GlobalChat.WriteCommandMessage(
+                                (keyValuePair.Value.safe ? "" : "<color=red>") +
+                                "/" + keyValuePair.Key +
+                                (keyValuePair.Value.safe ? "" : "</color>")
+                            );
+                        }
+                        GlobalChat.WriteCommandMessage("");
+                    }
+                } else if(commands.TryGetValue(args[0], out ModdedCommand cmd)) {
+                    if(cmd.safe) {
+                        cmd.Execute(args);
+                    } else {
+                        GlobalChat.WriteCommandMessage("/" + args[0] + " is intended for testing and is very likely to break your session. Are you sure you want to do this? (y/n)");
+                        commandToConfirm = cmd;
+                        argsToConfirm = args;
+                        cmdNameToConfirm = args[0];
+                    }
+                } else return false;
+            } else if(commandToConfirm != null && argsToConfirm != null) {
+                if(command == "n" || command == "y") {
+                    GlobalChat.Pester(MultiplayerSettings.playerName + " decided to run the seriously dangerous command /" + cmdNameToConfirm, false);
+                    commandToConfirm.Execute(argsToConfirm);
+                } else {
+                    GlobalChat.WriteCommandMessage("Cancelled command.");
+                }
+                commandToConfirm = null;
+                argsToConfirm = null;
+                cmdNameToConfirm = "";
+            } else return false;
             return true;
         }
     }
